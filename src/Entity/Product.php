e8196f61ba94ec7439bfe42773c41c6a835e95eb
tomcdj71 <contact.tomc@yahoo.fr>
2023-07-25 10:13:35 +0200

    hotfix: remove duplicate Product entity

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -13,12 +13,43 @@ class Product
     #[ORM\Column]
     private ?int $id = null;
 
+    
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+    
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+    
     #[ORM\Column]
     private ?float $price = null;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
     }
 
     public function getPrice(): ?float
