c4c78d1afbd37a4b8c008b963c1ad2f94e1cfd76
tomcdj71 <contact.tomc@yahoo.fr>
2023-07-25 09:34:30 +0200

    enh: add missing Price field in Product entity (#16)

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -13,8 +13,23 @@ class Product
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column]
+    private ?float $price = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
     }
 }
